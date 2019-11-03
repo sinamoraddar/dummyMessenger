63b8abc4d4a13d5274953c5727aa9baefd5d4672
sinamoraddar <sinamoraddar@gmail.com>
2019-11-03 15:46:07 +0330

    create the basic folder structure

---
+++ src/Components/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.test}>just testing!!!</div>
+  );
+}
+
+export default App;
