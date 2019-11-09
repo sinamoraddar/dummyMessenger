6fba90958f8266396ce22b7d683638e2c54bdd9d
sinamoraddar <sinamoraddar@gmail.com>
2019-11-09 19:29:10 +0330

    rename the users file to contacts

---
+++ src/Utils/contacts.js
@@ -0,0 +1,90 @@
+export const contacts = {
+  // 0 property is for the current user
+  0: {
+    name: "dennis bergkamp"
+  },
+  1: {
+    name: "sina moraddar",
+    profilePic: "",
+    messages: [
+      {
+        author: 1,
+        content: `
+        
+GENERATED LOREM IPSUM
+20
+  
+COPY  Icon copy  red
+ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat
+        `,
+        submitionTime: 150
+      },
+      {
+        author: 1,
+        content: `
+        
+GENERATED LOREM IPSUM
+20
+  
+COPY  Icon copy  red
+ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat
+        `,
+        submitionTime: 1500
+      },
+      {
+        author: 0,
+        content: `
+        
+GENERATED LOREM IPSUM
+20
+  
+COPY  Icon copy  red
+ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat
+        `,
+        submitionTime: 555000
+      }
+    ]
+  },
+  2: {
+    name: "diogo jota",
+    profilePic: "",
+    messages: [
+      {
+        author: 2,
+        content: `
+        
+GENERATED LOREM IPSUM
+20
+  
+COPY  Icon copy  red
+ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat
+        `,
+        submitionTime: 150
+      },
+      {
+        author: 2,
+        content: `
+        
+GENERATED LOREM IPSUM
+20
+  
+COPY  Icon copy  red
+ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat
+        `,
+        submitionTime: 1500
+      },
+      {
+        author: 0,
+        content: `
+        
+GENERATED LOREM IPSUM
+20
+  
+COPY  Icon copy  red
+ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat
+        `,
+        submitionTime: 555000
+      }
+    ]
+  }
+};
