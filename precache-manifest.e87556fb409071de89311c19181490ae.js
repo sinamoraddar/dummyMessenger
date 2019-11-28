3e9f3c9748212daa2f9b39a9c4ab9dae453f6891
sinamoraddar <sinamoraddar@gmail.com>
2019-11-28 19:39:19 +0330

    Updates

---
+++ precache-manifest.e87556fb409071de89311c19181490ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497030bb8edb3bb8b2b4cd997bad421b",
+    "url": "/dummyMessenger/index.html"
+  },
+  {
+    "revision": "f1811402c9ab1ea8d7ba",
+    "url": "/dummyMessenger/static/css/main.a8d2cb9f.chunk.css"
+  },
+  {
+    "revision": "cd305687d44264ea0f39",
+    "url": "/dummyMessenger/static/js/2.fc3c3a66.chunk.js"
+  },
+  {
+    "revision": "f1811402c9ab1ea8d7ba",
+    "url": "/dummyMessenger/static/js/main.7b5a6112.chunk.js"
+  },
+  {
+    "revision": "3f05b6dd98684a1123d9",
+    "url": "/dummyMessenger/static/js/runtime-main.00e15514.js"
+  }
+]);
